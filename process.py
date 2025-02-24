3ba640ca6c5c134ac40139fe6a10644ab493a101
owenpadgett <owen.padgett@uconn.edu>
2025-02-24 13:15:41 +0000

    added docstrings

--- process.py
+++ process.py
@@ -2,13 +2,16 @@
 
 class Process():
     def __init__(self, pid, cycles=100):
+        """Initializes a Process instance."""
         self.pid = pid
         self.cycles = cycles
         self.link = None
         self.prev = None
 
     def __eq__(self, other):
+        """Checks equality of two Processes."""
         return self.pid == other.pid
     
     def __repr__(self):
+        """Returns a useful string form of the Process."""
         return f"Process({self.pid}, {str(self.cycles)})"
