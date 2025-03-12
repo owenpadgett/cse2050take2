329ec0fd997767d29a42d73261dea831fcdd91db
owenpadgett <owen.padgett@uconn.edu>
2025-03-12 19:08:29 +0000

    new answers

--- answers.py
+++ answers.py
@@ -9,11 +9,11 @@
 # Run this file locally to verify you spelled everything correctly.           #
 ###############################################################################
 
-answers = {'alg_a': 'insertion',
+answers = {'alg_a': 'selection',
            'alg_b': 'quick',
            'alg_c': 'bubble',
            'alg_d': 'merge',
-           'alg_e': 'selection'
+           'alg_e': 'insertion'
           }
 
 # Do not edit anything below this line. This checks that you spelled all answers
